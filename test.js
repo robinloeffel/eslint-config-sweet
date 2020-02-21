59bfa5a32bb281888ee1d5e8d49d7911617e7a32
Robin Löffel <hi@robinloeffel.ch>
2020-02-21 19:00:39 +0100

    add a whole bunch of new rules and a test script

---
+++ test.js
@@ -0,0 +1,11 @@
+let arr= [ 1, 2, 3, 4,].map(async(number )=>{
+    var string =  "string"
+
+    console.
+        log ({number: number,
+            string
+    });
+
+    console.log( 1, 2 ,
+        4);
+});
